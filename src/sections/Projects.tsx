541cc45b48faaf8ec785ed54db55f475453d98c8
smrutiofficial <smrutiprakashrout3@gmail.com>
2024-08-26 11:12:54 +0530

    Initial commit

---
+++ src/sections/Projects.tsx
@@ -0,0 +1,118 @@
+import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
+import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
+import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
+import { Result } from "postcss";
+import Image from "next/image";
+import CheckIcon from "@/assets/icons/check-circle.svg";
+import Arrowup from "@/assets/icons/arrow-up-right.svg";
+import GrainImage from "@/assets/images/grain.jpg";
+
+const portfolioProjects = [
+  {
+    company: "Acme Corp",
+    year: "2022",
+    title: "Dark Saas Landing Page",
+    results: [
+      { title: "Enhanced user experience by 40%" },
+      { title: "Improved site speed by 50%" },
+      { title: "Increased mobile traffic by 35%" },
+    ],
+    link: "https://youtu.be/4k7IdSLxh6w",
+    image: darkSaasLandingPage,
+  },
+  {
+    company: "Innovative Co",
+    year: "2021",
+    title: "Light Saas Landing Page",
+    results: [
+      { title: "Boosted sales by 20%" },
+      { title: "Expanded customer reach by 35%" },
+      { title: "Increased brand awareness by 15%" },
+    ],
+    link: "https://youtu.be/7hi5zwO75yc",
+    image: lightSaasLandingPage,
+  },
+  {
+    company: "Quantum Dynamics",
+    year: "2023",
+    title: "AI Startup Landing Page",
+    results: [
+      { title: "Enhanced user experience by 40%" },
+      { title: "Improved site speed by 50%" },
+      { title: "Increased mobile traffic by 35%" },
+    ],
+    link: "https://youtu.be/Z7I5uSRHMHg",
+    image: aiStartupLandingPage,
+  },
+];
+
+export const ProjectsSection = () => {
+  return (
+    <section className="pb-16 lg:py-24">
+      <div className="container">
+        <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text flex justify-center">
+          Real-World Results
+        </p>
+        <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">
+          Featured Projects
+        </h2>
+        <p className="text-center md:text-lg lg:text-xl text-white/60 mt-4 max-w-md mx-auto">
+          See how i transformed concepts into enging digital experinces.
+        </p>
+        <div className="flex flex-col md:mt-20 mt-10 gap-20">
+          {/*  */}
+          {portfolioProjects.map((project) => (
+            <div
+              key={project.title}
+              className="bg-gray-800 rounded-3xl z-0 overflow-hidden after:z-10 relative after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8 md:pt-12 md:px-10 after:pointer-events-none lg:pt-16 lg:px-20"
+            >
+              <div
+                className="absolute inset-0 -z-10 opacity-5"
+                style={{ backgroundImage: `url(${GrainImage.src})` }}
+              ></div>
+              <div className="lg:grid lg:grid-cols-2 lg:gap-16">
+                <div className="lg:pb-16">
+                  <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
+                    <span>{project.company}</span>
+                    <span>{project.year}</span>
+                  </div>
+
+                  <h3 className="font-serif text-2xl mt-2 md:text-4xl md:mt-5">
+                    {project.title}
+                  </h3>
+                  <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
+                  <ul>
+                    {project.results.map((result, index) => (
+                      <li
+                        key={index}
+                        className="flex gap-2 text-sm md:text-base text-white/50 mt-4"
+                      >
+                        <CheckIcon className="size-5 md:size-6" />
+                        <span>{result.title}</span>
+                      </li>
+                    ))}
+                  </ul>
+                  <a href={project.link}>
+                    <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl inline-flex items-center justify-center gap-2 mt-8">
+                      <span>View Live Site</span>
+                      <Arrowup className="size-4" />
+                    </button>
+                  </a>
+                </div>
+                <div className="relative">
+                  <Image
+                    src={project.image}
+                    alt={project.title}
+                    className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:h-full lg:absolute lg:w-auto lg:max-w-none"
+                  />
+                </div>
+              </div>
+            </div>
+          ))}
+
+          {/*  */}
+        </div>
+      </div>
+    </section>
+  );
+};
