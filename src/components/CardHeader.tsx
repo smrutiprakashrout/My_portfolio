22f2555fb4f3b4592b0c797b15efbcab8cdae8b2
smrutiofficial <smrutiprakashrout3@gmail.com>
2024-09-03 23:23:04 +0530

    added contact section

--- src/components/CardHeader.tsx
+++ src/components/CardHeader.tsx
@@ -11,7 +11,7 @@ export const CardHeader = ({
   className?: string;
 }) => {
   return (
-    <div className={twMerge("flex flex-col p-6 md:py-8 md:py-10", className)}>
+    <div className={twMerge("flex flex-col p-6 md:py-8 md:px-10", className)}>
       <div className="inline-flex items-center gap-2">
         <StarIcon className="size-9 text-emerald-300" />
         <h3 className="font-serif text-3xl">{title}</h3>
