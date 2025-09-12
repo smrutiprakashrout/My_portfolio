239416b92d4e97d8bf67521b09d97a4ea0525a4e
smrutiofficial <smrutiprakashrout3@gmail.com>
2025-09-12 09:35:09 +0530

    seo sitemap fix

--- My_portfolio/src/app/sitemap.ts
+++ My_portfolio/src/app/sitemap.ts
@@ -1,15 +1,21 @@
 import { MetadataRoute } from 'next';
- 
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: 'https://devsmruti.vercel.app/',
       lastModified: new Date(),
       changeFrequency: 'weekly',
-      priority: 1,
+      priority: 1.0,
     },
     {
       url: 'https://devsmruti.vercel.app/about',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://devsmruti.vercel.app/contact',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
